4abf9c1fe4119d04db685eb45c16eb00d3ce474b
Eduardo's Mac <phelipee06@gmail.com>
2025-03-20 11:04:53 -0300

    Separating the logic of examination verification and confirmation

--- 2_O_OCP/ocp_example.py
+++ 2_O_OCP/ocp_example.py
@@ -2,45 +2,62 @@ from abc import ABC, abstractmethod
 
 
 # Versão com OCP
-class AprovaExame(ABC):
-    @abstractmethod
-    def aprovar_exame(self, exame): pass
-
-    @abstractmethod
-    def verificar_condicoes(self, exame): pass
-
 
 class Exame:
     def __init__(self, tipo: str):
         self.tipo = tipo
 
-    def aprovar(self, aprovador):
-        aprovador.aprovar_exame(self)
+
+class AprovaExame(ABC):
+    @abstractmethod
+    def aprovar_exame(self, exame): pass
+
+
+class VerificarTipoExame(ABC):
+    @abstractmethod
+    def verificar(self, exame: Exame) -> bool: pass
+
+
+class VerificarExameDeSangue(VerificarTipoExame):
+    def verificar(self, exame: Exame) -> bool:
+        return exame.tipo == 'sangue'
+
+
+class VerificarExameDeRaioX(VerificarTipoExame):
+    def verificar(self, exame: Exame) -> bool:
+        return exame.tipo == 'raio-x'
 
 
 class AprovadorExameDeSangue(AprovaExame):
-    def aprovar_exame(self, exame):
-        if exame.tipo == "sangue" and self.verificar_condicoes(exame):
-            print("Exame sanguíneo aprovado!")
+    def __init__(self, verifica: VerificarExameDeSangue):
+        self.verifica = verifica
 
-    def verificar_condicoes(self, exame):
-        return True
+    def aprovar_exame(self, exame):
+        if self.verifica.verificar(exame):
+            print(f"Exame de {exame.tipo} aprovado!")
+        else:
+            print(f"Exame de {exame.tipo} não aprovado!")
 
 
 class AprovadorExameDeRaioX(AprovaExame):
-    def aprovar_exame(self, exame):
-        if exame.tipo == "raio-x" and self.verificar_condicoes(exame):
-            print("Raio-X aprovado!")
+    def __init__(self, verifica: VerificarExameDeRaioX):
+        self.verifica = verifica
 
-    def verificar_condicoes(self, exame):
-        return True
+    def aprovar_exame(self, exame):
+        if self.verifica.verificar(exame):
+            print(f"Exame de {exame.tipo} aprovado!")
+        else:
+            print(f"Exame de {exame.tipo} não aprovado!")
 
 
 exame_sangue = Exame("sangue")
 exame_raio_x = Exame("raio-x")
 
-aprovador_sangue = AprovadorExameDeSangue()
-aprovador_raio_x = AprovadorExameDeRaioX()
+verificar_sangue = VerificarExameDeSangue()
+verificar_raio_x = VerificarExameDeRaioX()
 
-exame_sangue.aprovar(aprovador_sangue)
-exame_raio_x.aprovar(aprovador_raio_x)
+aprovar_sangue = AprovadorExameDeSangue(verificar_sangue)
+aprovar_raio_x = AprovadorExameDeRaioX(verificar_raio_x)
+
+aprovar_sangue.aprovar_exame(exame_sangue)
+aprovar_raio_x.aprovar_exame(exame_raio_x)
